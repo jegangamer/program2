fccd4396053a5dcf4e3c30ba2844bc20c2ba16fa
jegangamer <30927225+jegangamer@users.noreply.github.com>
2017-08-12 09:47:20 -0700

    Rename evenorodd.java to even.java

---
+++ even.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class evenorodd
+{
+public static void main(String[] args)
+{
+Scanner m=new Scanner(System.in);
+int p=m.nextInt();
+if((p%2)==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}}
